d80460853098656c2093f81915b91bd4443ffd23
sebaspb234 <62064772+sebaspb234@users.noreply.github.com>
2020-06-15 23:16:54 -0500

    Add files via upload

---
+++ src/Poblacion.cpp
@@ -0,0 +1,106 @@
+#include "Poblacion.h"
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+Poblacion::Poblacion(string miNombre,string miApellido,string miDistrito,string _sintoma,string _antecedente,int miEdad)
+{
+    nombrePaciente=miNombre;
+    apellidoPaciente=miApellido;
+    distrito=miDistrito;
+    sintoma=_sintoma;
+    antecedente=_antecedente;
+    edad=miEdad;
+}
+
+void Poblacion::setNombre(string miNombre)
+{
+    nombrePaciente=miNombre;
+}
+
+void Poblacion::setApellido(string miApellido)
+{
+    apellidoPaciente=miApellido;
+}
+
+string Poblacion::getNombre()
+{
+    return nombrePaciente;
+}
+
+string Poblacion::getApellido()
+{
+    return apellidoPaciente;
+}
+
+string Poblacion::Registro()
+{
+    cout<<"같같Ingresa tu informacion같같"<<endl;
+    cout<<"Ingresa el nombre del paciente: ";getline(cin,nombrePaciente);
+    cout<<"Ingresa el apellido del paciente: ";getline(cin,apellidoPaciente);
+    cout<<"Ingresa la edad del paciente: ";cin>>edad; cin.ignore();
+    cout<<"Ingresa el distrito del paciente: ";getline(cin,distrito);
+    setNombre(nombrePaciente);setApellido(apellidoPaciente);setEdad(edad);setDistrito(distrito);
+}
+
+string Poblacion::mostrarDatos()
+{
+    cout<<"DATOS DEL PACIENTE: \n";
+    cout<<"Nombre: "<<getNombre()<<endl;
+    cout<<"Apellido: "<<getApellido()<<endl;
+    cout<<"Edad: "<<getEdad()<<endl;
+    cout<<"Distrito: "<<getDistrito()<<endl;
+}
+string Poblacion::Sintomas()
+{
+    cout<<"Ingresa los sintomas que presenta el paciente: ";getline(cin,sintoma);
+    setSintomas(sintoma);
+    cout<<"Los sintomas que presenta el paciente son: "<<getSintomas()<<endl;
+}
+
+string Poblacion::AntecedentesMedicos()
+{
+    cout<<"Ingresa los antecedentes medicos del paciente: ";getline(cin,antecedente);
+    setAntecedentes(antecedente);
+    cout<<"Los antecedentes medicos del paciente son: "<<getAntecedente()<<endl;
+}
+
+void Poblacion::setSintomas(string _sintoma)
+{
+    sintoma=_sintoma;
+}
+
+void Poblacion::setEdad(int miEdad)
+{
+    edad=miEdad;
+}
+
+int Poblacion::getEdad()
+{
+    return edad;
+}
+
+void Poblacion::setDistrito(string miDistrito)
+{
+    distrito=miDistrito;
+}
+
+void Poblacion::setAntecedentes(string _antecedente)
+{
+    antecedente=_antecedente;
+}
+
+string Poblacion::getDistrito()
+{
+    return distrito;
+}
+
+string Poblacion::getSintomas()
+{
+    return sintoma;
+}
+
+string Poblacion::getAntecedente()
+{
+    return antecedente;
+}
---
+++ src/Vulnerable.cpp
@@ -0,0 +1,66 @@
+#include "Vulnerable.h"
+#include <iostream>
+#include <string.h>
+#include <fstream>
+using namespace std;
+
+
+Vulnerable::Vulnerable()
+{
+   centroMedico="";
+}
+
+
+void Vulnerable::setCentroMedico(string centroM)
+{
+    centroMedico=centroM;
+}
+
+string Vulnerable::getCentroMedico()
+{
+    return centroMedico;
+}
+
+string Vulnerable::centroRecomendado()
+{
+    cout<<"\nEl paciente debe asistir al Hospital Goyeneche\n";
+}
+
+void Vulnerable::registro_Paciente()
+{
+    ofstream archivo;
+    archivo.open("Pacientes_Vulnerables",ios::app|ios::out);
+    if(archivo.fail()){cout<<"No pudo abrirse el archivo"; exit(1);}
+    archivo<<nombrePaciente<<" "<<apellidoPaciente<<" "<<edad<<" "<<distrito<<endl;
+    archivo.close();
+}
+
+void Vulnerable::mostrar_Registro()
+{
+    ifstream leerfile;
+    leerfile.open("Pacientes_Vulnerables",ios::in);
+    if(leerfile.is_open())
+    {
+        cout<<"Registro de paciente"<<endl;
+        leerfile>>nombrePaciente;
+        while(!leerfile.eof())
+        {
+            leerfile>>apellidoPaciente;
+            leerfile>>edad;
+            leerfile>>distrito;
+            cout<<"Nombre: "<<nombrePaciente<<endl;
+            cout<<"Apellido: "<<apellidoPaciente<<endl;
+            cout<<"Edad: "<<edad<<endl;
+            cout<<"Distrito: "<<distrito<<endl;
+            leerfile>>nombrePaciente;
+        }
+        leerfile.close();
+    }else{cout<<"El archivo no pudo abrirse\n";}
+
+}
+
+
+
+
+
+
